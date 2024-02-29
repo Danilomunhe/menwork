1c58145b8e349f2863fa8e809dee913d8975becc
Danilo <danilomunhe@gmail.com>
2024-02-29 10:25:38 -0300

    menu-burger e remocoes do fade left

--- assets/js/script.js
+++ assets/js/script.js
@@ -387,3 +387,24 @@ const servicos = {
     alternativo: false,
   },
 };
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const closeButton = document.querySelector(".close-button-menu");
+  const itemsMenu = document.querySelector(".itens-menu");
+
+  function toggleMenu() {
+    if (window.innerWidth <= 768) {
+      itemsMenu.style.display =
+        itemsMenu.style.display === "none" || itemsMenu.style.display === ""
+          ? "flex"
+          : "none";
+    } else {
+      itemsMenu.style.display = "flex";
+    }
+  }
+  burgerMenu.addEventListener("click", toggleMenu);
+  window.addEventListener("resize", toggleMenu);
+  closeButton.addEventListener("click", function () {
+    itemsMenu.style.display = "none";
+  });
+});
